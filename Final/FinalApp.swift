fbce23c7d5bb65ab57d83949f32f89815a7a7b84
maryamMH100 <mar0iam100@gmail.com>
2024-10-07 21:14:50 +0300

    Initial Commit

---
+++ Final/FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalApp.swift
+//  Final
+//
+//  Created by Maryam on 07/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
